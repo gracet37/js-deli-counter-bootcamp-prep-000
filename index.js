c9a5c51fad8932581e9b2e868b10953e3915f6ae
Grace Tan <gracetan37@gmail.com>
2019-02-19 06:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function takeANumber(katzDeliLine,name) {
 function nowServing(katzDeliLine) {
   for (let i=0; i< katzDeliLine.length; i++);
   if (katzDeliLine.length === 0) {
-    return`There is nobody waiting to be served!`
+    console.log("There is nobody waiting to be served!")
   }
   else {
   return `Currently serving ${katzDeliLine.shift()}.`;
@@ -21,7 +21,7 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   for (var i = 0; i < katzDeliLine.length; i++);
   if (katzDeliLine.length === 0) {
-    return`The line is currently empty.`
+    console.log("The line is currently empty.")
   }
   else {
     var x = [];
