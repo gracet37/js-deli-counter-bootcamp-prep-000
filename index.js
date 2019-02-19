6050f5a7d6935e97958ff4bd87977e06c040a8d4
Grace Tan <gracetan37@gmail.com>
2019-02-19 07:19:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   for (let i=0; i< katzDeliLine.length; i++);
   if (katzDeliLine.length === 0) {
-    return `The line is currently empty`;
+    return `The line is currently empty.`;
   }
   else {let i=0; katzDeliLine[i] = i+1+ ". " + katzDeliLine[i];
   katzDeliLine.shift("The line is currently: ");
