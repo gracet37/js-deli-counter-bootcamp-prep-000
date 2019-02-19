ec9587eba58307f4e3b53f748670007f7668d1da
Grace Tan <gracetan37@gmail.com>
2019-02-19 07:32:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function currentLine(katzDeliLine) {
   }
   else {
   for (let i=0; i< katzDeliLine.length; i++) {
-    katzDeliLine[i] = i + ". " + katzDeliLine[i];
+    katzDeliLine[i] = i + katzDeliLine[i];
     katzDeliLine.unshift('The line is currently: ');
     return katzDeliLine;
   }
