23e705fcb7196b80397a61c9bdb7c09038b4b170
Grace Tan <gracetan37@gmail.com>
2019-02-19 06:15:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeliLine = []
+// Creating an empty array. This represents the beginning of the day, where there are no people
+
+// Build take a number function - tailored to name and number & return length of the array
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name); //new customer at the end of the line
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.` 
+}
+
+// Create a loop to run - chose for as business continues to operate and therefore requires the loop to keep running. 
+function nowServing(katzDeliLine) {
+  for (let i=0; i< katzDeliLine.length; i++);
+  if (katzDeliLine.length === 0) {
+    return`There is nobody waiting to be served!`
+  }
+  else {
+  return `Currently serving ${katzDeliLine.shift()}.`;
+}
+}
+
+function currentLine(katzDeliLine) {
+  for (var i = 0; i < katzDeliLine.length; i++);
+  if (katzDeliLine.length === 0) {
+    return`The line is currently empty.`
+  }
+  else {
+    var x = [];
+    x.push("The line is currently:" + (i+1) + ". " + katzDeliLine[i]);
+  return x;
+}
+}
