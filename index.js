5f824c188f02569ef0aa6a07cf7675ead5742410
Grace Tan <gracetan37@gmail.com>
2019-02-19 08:25:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,14 +19,16 @@ function nowServing(katzDeliLine) {
 }
 }
 
+
 function currentLine(katzDeliLine) {
    if (katzDeliLine.length === 0) {
     return `The line is currently empty.`;
   }
   else {
   for (let i=0; i< katzDeliLine.length; i++) {
-    katzDeliLine = i+1 + ". " + katzDeliLine[i] + ",";
-    return `The line is currently: ${katzDeliLine.shift()}`;
+    var katzDeli =[]
+    katzDeli.push(i+1 + ". " + katzDeliLine[i];
+    return `The line is currently: ${katzDeli}`;
   }
 }
 }
